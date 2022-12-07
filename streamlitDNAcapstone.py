7e9e3c5bc4914ce46423e7f1e1600d8756d8d7b6
EBC33 <59582165+ebc33@users.noreply.github.com>
2022-12-07 02:36:33 -0500

    Added modelSequenceDNA function

--- streamlitDNAcapstone.py
+++ streamlitDNAcapstone.py
@@ -1,6 +1,26 @@
+def trainEval(model,xtrain,ytrain,bsize,epochs,xtest,ytest):
+    batch_size=bsize
+    historyTrain=model.fit(xtrain,ytrain,batch_size=bsize,epochs=epochs)
+    historyEval=model.fit(xtrain,ytrain,batch_size=bsize,epochs=epochs,validation_data=(xtest,ytest))
+
+def modelSequenceDNA(xtrain=x_train_array,ytrain=y_train_array,xtest=x_test_array,ytest=y_test_array,bsize=44,epochs=3,inputSize=33289,sizeHL=128,activnHL='exponential',activnOP='softmax',nLayers=3,nClass=2,opt='adam',loss='sparse_categorical_crossentropy',metrics=[keras.metrics.SparseCategoricalAccuracy(name='acc')]):
+    model=tf.keras.Sequential()
+    model.add(tf.keras.Input(shape=inputSize,))
+    for i in range(nLayers):
+        model.add(tf.keras.layers.Dense(sizeHL,activation=activnHL))
+    model.add(layers.Dense(nClass,activation=activnOP))
+    model.summary()
+    model.compile(optimizer=opt,loss=loss,metrics=metrics)
+    trainEval(model,xtrain,ytrain,bsize,epochs,xtest,ytest)
+    return model    
+
 #sample streamlit file
 import streamlit as st
 import pandas as pd
+import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow import keras
+
 st.markdown('# Verify ID with DNA')
 st.sidebar.markdown('# Verify unseen original face with DNA')
 
@@ -20,6 +40,8 @@ with right_column:
         uploaded_DNA=st.file_uploader("uploaded_file") 
         if uploaded_DNA is not None:
             dataDNAup=pd.read_csv(uploaded_DNA)
-            st.write(dataDNAup)
+            model=modelSequenceDNA(xtrain=xtrain.txt,ytrain=ytrain.txt,xtest=xtest.txt,ytest=ytest.txt)
+            model.predict(dataDNAup)
+            #st.write(dataDNAup)
     elif chosen=="No":
         st.write("Not so sure...would you like to reenter a guess?")

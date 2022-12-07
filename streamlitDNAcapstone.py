03044eaf57095075f67f4f6825b40b99e202c160
EBC33 <59582165+ebc33@users.noreply.github.com>
2022-12-06 19:07:55 -0500

    Added uploadfile

--- streamlitDNAcapstone.py
+++ streamlitDNAcapstone.py
@@ -1,19 +1,23 @@
 #sample streamlit file
 import streamlit as st
-st.markdown('# Main Page')
-st.sidebar.markdown('# Main Page')
+st.markdown('# Verify ID with DNA')
+st.sidebar.markdown('# Verify unseen original face with DNA')
 
 left_column,right_column=st.columns(2)
 
 with left_column:
-    chosen=st.sidebar.selectbox("Please select your guess on type of feature this DNA visualizes", ["pointy","non-pointy"])
-    st.write(f"You think the owner of this DNA has a {chosen} feature.") 
+    chosen=st.sidebar.selectbox("Please guess type of feature this DNA visualizes", ["pointy","non-pointy"])
+    st.write(f"You think owner of this DNA has a {chosen} feature.") 
 
 with right_column:
     chosen=st.slider("Select probability that your guess is right",0,100,10)
     st.write(f"You are {chosen}% sure of your guess...")
     chosen=st.sidebar.selectbox("Would you like to verify guess?",["Yes","No"])
     if chosen=="Yes":
-        st.write("Please click on this button.") #to view code - st.code(get_file_content_as_string("streamlit_app.py"))
+        #st.write("Please click on this button.") #to view code - st.code(get_file_content_as_string("streamlit_app.py"))
+        st.write("Please upload DNA on feature of interest. File must be of .txt or .csv type")
+        uploaded_DNA=st.file_uploader("uploaded_file")
+        dataDNAup=pd.read_csv(uploaded_DNA)
+        st.write(dataDNAup)
     elif chosen=="No":
         st.write("Not so sure...would you like to reenter a guess?")
